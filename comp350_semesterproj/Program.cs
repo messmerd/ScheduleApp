f3e2b9b8239e71a0a5fcf622dad722ec149c66e6
messmerd <messmerd97@gmail.com>
2018-03-18 15:50:15 -0400

    Set things up as a Visual Studio project so the program can be worked on easily

---
+++ comp350_semesterproj/Program.cs
@@ -0,0 +1,236 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using SpellingCorrector;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        // This code is for testing the course search and spell-checking features 
+
+        // Creates an instance of the Search class using course_dictionary.txt for spell-checking and mini course database 
+        Search search = new Search("course_dictionary.txt");
+
+        Console.WriteLine("Type a course name or course code and press Enter to search for it. \nPress Enter without typing anything to exit.\n ");
+
+        string query = " ";
+        List<Course> searchResults;
+        int i = 0;
+
+        // This code is for 
+        while (query.Length != 0)
+        {
+            query = Console.ReadLine();
+            Console.WriteLine();
+            if (query.Length == 0)
+                break;
+            search.searchForQuery(query);
+            searchResults = search.lastSearchResults.getCourses();
+
+            if (searchResults.Count != 0)
+            {
+                i = 0;
+                if (search.lastSearchResults.getCorrectedQuery() != query.ToLower())  // If your query had a spelling mistake
+                    Console.WriteLine("Did you mean {0}?", search.lastSearchResults.getCorrectedQuery());
+                Console.WriteLine("Top 20 search results for {0}, ordered by best match: ", query);
+                foreach (var match in searchResults)
+                {
+                    Console.WriteLine("CourseID = {0}, relevance = {1}", match.getCourseID(), search.lastSearchResults.getCourseRelevance()[i]);
+                    if (i == 19) { break; }  // Only show top 20 search results
+                    i++;
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("No course was found for the query {0}. \n", query);
+            }
+        }
+
+    }
+
+}
+
+
+
+public class Search
+{
+    private advancedOptions options;   // For storing advanced search options 
+    private Spelling spelling;         // Instance of Spelling object from Spelling.cs 
+    private string courseDictionary;   // Stores course dictionary filename for use by spellchecker and course search 
+    public results lastSearchResults;  // Stores the search results after using searchForQuery()
+
+    public struct results
+    {
+        public List<Course> courses;       // Courses ordered from most relevant to least 
+        public string query;               // The query passed to searchForQuery()
+        public string correctedQuery;      // The spell-checked version of query
+        public List<int> courseRelevance;  // Stores the relevance of each course in the results. courseRelevance[i] = relevance of courses[i] 
+
+        // Getters: 
+        public List<Course> getCourses()
+        {
+            return courses;
+        }
+        public string getCorrectedQuery()
+        {
+            return correctedQuery;
+        }
+        public string getQuery()
+        {
+            return query;
+        }
+        public List<int> getCourseRelevance()
+        {
+            return courseRelevance;
+        }
+
+    }
+
+    private struct advancedOptions
+    {
+        public int rmp; // Rate My Professor rating 
+        // Put other advanced options here later 
+    }
+
+    public Search()  // Constructor 
+    {
+        this.spelling = new Spelling("course_dictionary.txt");
+        this.courseDictionary = "course_dictionary.txt";
+
+        this.lastSearchResults.query = "";
+        this.lastSearchResults.correctedQuery = "";
+        this.lastSearchResults.courses = new List<Course>();
+        this.lastSearchResults.courseRelevance = new List<int>();
+
+        this.options.rmp = 0; // Rate my professor rating
+        // Other advanced options will go here 
+    }
+
+    public Search(string courseDictionary)  // Constructor 
+    {
+        this.spelling = new Spelling(courseDictionary);
+        this.courseDictionary = courseDictionary;
+
+        this.lastSearchResults.query = "";
+        this.lastSearchResults.correctedQuery = "";
+        this.lastSearchResults.courses = new List<Course>();
+        this.lastSearchResults.courseRelevance = new List<int>();
+
+        this.options.rmp = 0;  // Rate my professor rating 
+        // Other advanced options will go here.
+    }
+
+    // This function creates a list of Courses that match a given query or are close enough to be recognized by the spell-checker. 
+    // It also creates a string containing the spell-checked version of the user's query. 
+    // All these search results are placed in the lastSearchResults struct, which can be accessed using the getters. 
+    // The query can be a course name or course code. The results are organized from best match to worst. Courses that don't match at all are not included. 
+    public void searchForQuery(string query)
+    {
+        string correctedQuery = "";   // Stores the spell-checked version of the user's query. 
+        List<List<int>> matching = new List<List<int>>();    // A list where each index represents a word in the query. For each word, there is a list of ints. These ints are the course IDs of courses containing that word in their name or course code. 
+        Dictionary<int, int> bestMatches = new Dictionary<int, int>(); // Maps course IDs to their relevance (how many of the words in the query match words in the course title or course code)
+        KeyValuePair<string, List<int>> result = new KeyValuePair<string, List<int>>(); // Stores the results from the spellchecker
+
+        List<string> querySplit = query.Split().ToList();
+
+        foreach (var word in querySplit)
+        {
+            // Needs tweaked:    ? 
+            if (word.Length > 4 || (querySplit.Count == 1 && word.Length > 2))   // Doesn't correct words smaller than 5 characters, unless it's a single word query. Doing so could lead to bad search results. 
+                result = spelling.CorrectExt(word, true);
+            else
+                result = spelling.CorrectExt(word, false);
+
+            // For single letter queries, list classes whose first letter in course name/code matches letter? 
+
+            correctedQuery += result.Key;
+            if (word != querySplit.Last())
+                correctedQuery += " ";
+
+            matching.Add(result.Value.GetRange(0, result.Value.Count));
+            if (matching.Last().Count != 0)
+                matching.Last().RemoveAt(0);
+        }
+
+        foreach (var match in matching)  // For each word of corrected query 
+        {
+            foreach (var id in match)    // For each course ID of courses that contain the corrected query word in their title or course code 
+            {
+                if (bestMatches.ContainsKey(id))
+                {
+                    bestMatches[id]++;  // Increment the number of matching words for the course "id"  
+                    // If it matches the department code rather than just part of the course title, maybe the relevance should be increased an extra amount for better results?   
+                }
+                else
+                {
+                    bestMatches[id] = 1; // This course has one matching word 
+                }
+                // Note: Courses whose titles or course codes match more words of the corrected query have a higher "relevance" and will appear at the top of search results. 
+            }
+        }
+
+        var bestMatchesList = bestMatches.ToList();
+        bestMatchesList.Sort((pair1, pair2) => pair2.Value.CompareTo(pair1.Value));  // Sorts the courses by relevance. Most relevant first. 
+
+        // Need function (a factory, maybe) to generate and return a course struct from a course id. 
+        // This could be done by a CourseInfo class, which acts as a database. 
+        // Or maybe it should just be done by a Class constructor that accesses the database class. It is being done this way for now. 
+
+        lastSearchResults.courseRelevance.Clear();  // Clear the results from the last search 
+        lastSearchResults.courses.Clear();          // Clear the results from the last search 
+
+        foreach (var course in bestMatchesList) // Iterate through the courses in the search results 
+        {
+            if (course.Value != 0)  // This condition is probably not needed. 
+            {
+                lastSearchResults.courses.Add(new Course(course.Key));  // Add the courses to lastSearchResults struct 
+                lastSearchResults.courseRelevance.Add(course.Value);    // Add the course relevances to lastSearchResults struct 
+            }
+            else
+                Console.WriteLine("Error!"); // Shouldn't ever happen. Could remove this later 
+        }
+
+        lastSearchResults.correctedQuery = correctedQuery; // Setting correctedQuery in the lastSearchResults struct 
+        lastSearchResults.query = query;                   // Setting correctedQuery in the lastSearchResults struct 
+    }
+
+    // Corrects a given course name or course code and returns the corrected version. 
+    public string Correct(string query)
+    {
+        return spelling.Correct(query);
+    }
+
+}
+
+public struct Course
+{
+    private int courseID;
+    private string courseName;
+    // Other attributes of the course go here
+
+    public Course(int courseID)  // Constructor
+    {
+        this.courseID = courseID;
+        //  ... (other Class attributes to be set here)
+        this.courseName = "<course name>";  // Temporary, of course
+        // We need to have a Course Database class that contains a function (a factory?) which helps create Course structs from course IDs. 
+
+    }
+
+    public string getCourseName()
+    {
+        return courseName;
+    }
+
+    public int getCourseID()  // Note: courseID is NOT the same as courseCode 
+    {
+        return courseID;
+    }
+
+}
+
---
+++ comp350_semesterproj/Spelling.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SpellingCorrector
+{
+    /// <summary>
+    /// Conversion from http://norvig.com/spell-correct.html by C.Small
+    /// Modified by Dalton Messmer 
+    /// </summary>
+    /// 
+
+    public class Spelling
+    {
+        private readonly Dictionary<String, List<int>> _dictionary = new Dictionary<String, List<int>>();
+        private static readonly Regex _wordRegex = new Regex("[a-z]+", RegexOptions.Compiled);
+
+        public Spelling(string dictionary_filename)
+        {
+            string fileContent = "";
+            try
+            {
+                fileContent = File.ReadAllText(dictionary_filename);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "\n");
+                return;
+            }
+
+            List<string> wordList = fileContent.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            int i = 0;
+
+            foreach (var word in wordList)
+            {
+                foreach (var word2 in word.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    string trimmedWord = word2.Trim().ToLower();
+                    if (_wordRegex.IsMatch(trimmedWord))
+                    {
+                        if (_dictionary.ContainsKey(trimmedWord))
+                        {
+                            _dictionary[trimmedWord][0]++;
+                            if (_dictionary[trimmedWord].Last() != i)  // So duplicates are not added 
+                                _dictionary[trimmedWord].Add(i);
+                        }
+                        else
+                        {
+                            _dictionary.Add(trimmedWord, new List<int> { 1, i });
+                        }
+                    }
+                }
+                i++;
+            }
+        }
+
+        public string CorrectSentence(string sentence)
+        {
+            if (string.IsNullOrEmpty(sentence))
+                return sentence;
+
+            var corrections = new List<string>();
+            foreach (string word in sentence.Split(' '))
+            {
+                corrections.Add(Correct(word));
+            }
+
+            return string.Join(" ", corrections);
+        }
+
+        public string Correct(string word)
+        {
+            return CorrectExt(word, true).Key;
+        }
+
+        public KeyValuePair<string, List<int>> CorrectExt(string word, bool useCorrection)
+        {
+            if (word != null)
+                word = word.ToLower();
+
+            KeyValuePair<string, List<int>> wordPair;
+            if (_dictionary.ContainsKey(word))
+            {
+                wordPair = new KeyValuePair<string, List<int>>(word, _dictionary[word]);
+            }
+            else
+            {
+                wordPair = new KeyValuePair<string, List<int>>(word, new List<int>());
+            }
+
+            if (useCorrection == false)
+                return wordPair;
+
+            if (string.IsNullOrEmpty(word))
+                return wordPair;
+
+            // known()
+            if (_dictionary.ContainsKey(word))
+                return wordPair;
+
+            List<String> list = Edits(word);
+            Dictionary<string, List<int>> candidates = new Dictionary<string, List<int>>();
+
+            foreach (string wordVariation in list)
+            {
+                if (_dictionary.ContainsKey(wordVariation) && !candidates.ContainsKey(wordVariation))
+                    candidates.Add(wordVariation, _dictionary[wordVariation]);
+            }
+
+            if (candidates.Count > 0)
+            {
+                return candidates.OrderByDescending(x => x.Value[0]).First();
+            }
+            // known_edits2()
+            foreach (string item in list)
+            {
+                foreach (string wordVariation in Edits(item))
+                {
+                    if (_dictionary.ContainsKey(wordVariation) && !candidates.ContainsKey(wordVariation))
+                        candidates.Add(wordVariation, _dictionary[wordVariation]);
+                }
+            }
+
+            return (candidates.Count > 0) ? candidates.OrderByDescending(x => x.Value[0]).First() : wordPair;
+        }
+
+        private List<string> Edits(string word)
+        {
+            var splits = new List<Tuple<string, string>>();
+            var transposes = new List<string>();
+            var deletes = new List<string>();
+            var replaces = new List<string>();
+            var inserts = new List<string>();
+
+            // Splits
+            for (int i = 0; i < word.Length; i++)
+            {
+                var tuple = new Tuple<string, string>(word.Substring(0, i), word.Substring(i));
+                splits.Add(tuple);
+            }
+
+            // Deletes
+            for (int i = 0; i < splits.Count; i++)
+            {
+                string a = splits[i].Item1;
+                string b = splits[i].Item2;
+                if (!string.IsNullOrEmpty(b))
+                {
+                    deletes.Add(a + b.Substring(1));
+                }
+            }
+
+            // Transposes
+            for (int i = 0; i < splits.Count; i++)
+            {
+                string a = splits[i].Item1;
+                string b = splits[i].Item2;
+                if (b.Length > 1)
+                {
+                    transposes.Add(a + b[1] + b[0] + b.Substring(2));
+                }
+            }
+
+            // Replaces
+            for (int i = 0; i < splits.Count; i++)
+            {
+                string a = splits[i].Item1;
+                string b = splits[i].Item2;
+                if (!string.IsNullOrEmpty(b))
+                {
+                    for (char c = 'a'; c <= 'z'; c++)
+                    {
+                        replaces.Add(a + c + b.Substring(1));
+                    }
+                    for (char c = '0'; c <= '9'; c++)
+                    {
+                        replaces.Add(a + c + b.Substring(1));
+                    }
+                }
+            }
+
+            // Inserts
+            for (int i = 0; i < splits.Count; i++)
+            {
+                string a = splits[i].Item1;
+                string b = splits[i].Item2;
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    inserts.Add(a + c + b);
+                }
+                for (char c = '0'; c <= '9'; c++)
+                {
+                    inserts.Add(a + c + b);
+                }
+            }
+
+            return deletes.Union(transposes).Union(replaces).Union(inserts).ToList();
+        }
+    }
+}
