25a3140c615edd54f65a75af02cb76ff43d8e77b
Michael Bender <bendermc1@gcc.edu>
2018-03-19 19:26:12 -0400

    Began work on Course struct as well as database import

---
+++ comp350_semesterproj/CourseInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace comp350_semesterproj
+{
+    class CourseInfo
+    {
+        List<Course> database;
+
+        public void parseCSV()
+        {
+            while (false)
+            {//CourseCode	ShortTitle	LongTitle	BeginTime	EndTime	Meets	Building	Room	Enrollment	Capacity
+                Course nextCourse;
+                
+
+                Build result;  //BAO, HAL, HH, OFFCP, PFAC, PLC, RH, RO, STEM, TBD, NONE
+                if (Enum.TryParse(myname, out result)) nextCourse.building = result;
+                else nextCourse.building = Build.NONE;
+                database.Add(nextCourse);
+            }
+        }
+    }
+}
