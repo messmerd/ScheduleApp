f9e08c8dd830ba780a16a87077e44f8ef12a6b2b
Michael Bender <bendermc1@gcc.edu>
2018-03-20 17:56:24 -0400

    work on importing excel file into course struct

--- ScheduleApp/ScheduleApp/Course.cs
+++ ScheduleApp/ScheduleApp/Course.cs
@@ -14,7 +14,7 @@ namespace CourseClass
 
         public string courseDept;
         public int courseNum;
-        public char courseSect;
+        public string courseSect;
         public string courseCode;// = courseDept + courseNum.toString();
 
         public string shortName;
@@ -29,15 +29,11 @@ namespace CourseClass
 
 
         private int courseID;
-        private string courseName;
-        // Other attributes of the course go here
 
         public Course(int courseID)  // Constructor
         {
             this.courseID = courseID;
-            //  ... (other Class attributes to be set here)
-            this.courseName = "<course name>";  // Temporary, of course
-                                                // We need to have a Course Database class that contains a function (a factory?) which helps create Course structs from course IDs. 
+
             this.professor = Tuple.Create<string, string>(null, null);
 
             this.time = new List<Tuple<double, double>> {
@@ -53,7 +49,7 @@ namespace CourseClass
 
             courseDept = "";
             courseNum = 0;
-            courseSect = 'A';
+            courseSect = "A";
             courseCode = courseDept + " " + courseNum + " " + courseSect;
 
             shortName = "";
@@ -63,9 +59,35 @@ namespace CourseClass
             capacity = 0;
         }
 
-        public string getCourseName()
+        public Course(List<string> parsedCourse) // Constructor
         {
-            return courseName;
+            this.courseID = 0;
+            
+            this.professor = Tuple.Create<string, string>(null, null);
+
+            this.time = new List<Tuple<double, double>> {
+                Tuple.Create<double, double>(0, 0),
+                Tuple.Create<double, double>(0, 0),
+                Tuple.Create<double, double>(0, 0),
+                Tuple.Create<double, double>(0, 0),
+                Tuple.Create<double, double>(0, 0)
+            };
+
+            Build result;//BAO, HAL, HH, OFFCP, PFAC, PLC, RH, RO, STEM, TBD, NONE
+            if (Enum.TryParse(parsedCourse[5], out result)) { building = result; }//fix building loc
+            else { building = Build.NONE; }
+            this.room = 0;
+
+            courseDept = "";
+            courseNum = 0;
+            courseSect = "A";
+            courseCode = courseDept + " " + courseNum + " " + courseSect;
+
+            shortName = "";
+            longName = "";
+
+            enrollment = 0;
+            capacity = 0;
         }
 
         public int getCourseID()  // Note: courseID is NOT the same as courseCode 
--- ScheduleApp/ScheduleApp/CourseInfo.cs
+++ ScheduleApp/ScheduleApp/CourseInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using CourseClass;
+using Excel = Microsoft.Office.Interop.Excel;       //Microsoft Excel 14 object in references-> COM tab
 
 namespace CourseInfoClass
 {
@@ -12,15 +13,27 @@ namespace CourseInfoClass
 
         void parseCSV()
         {
-            string parsedIn;
-            while (false)
-            {//CourseCode	ShortTitle	LongTitle	BeginTime	EndTime	Meets	Building	Room	Enrollment	Capacity
-                Course nextCourse;
-                
+            //https://coderwall.com/p/app3ya/read-excel-file-in-c
 
-                Build result;  //BAO, HAL, HH, OFFCP, PFAC, PLC, RH, RO, STEM, TBD, NONE
-                if (Enum.TryParse(parsedIn, out result)) { nextCourse.building = result; }
-                else { nextCourse.building = Build.NONE; }
+            //Create COM Objects. Create a COM object for everything that is referenced
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"sandbox_test.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = 762;
+            int colCount = 10;
+
+            //iterate over the rows and columns and print to the console as it appears in the file
+            //excel is not zero based!!
+            for (int i = 2; i <= rowCount; i++)//CourseCode	ShortTitle	LongTitle	BeginTime	EndTime	Meets	Building	Room	Enrollment	Capacity
+            {
+                List<string> parsedCourse = new List<string>();
+                for (int j = 1; j <= colCount; j++)
+                {
+                    parsedCourse.Add(xlRange.Cells[i, j].Value2.ToString());   
+                }
+                Course nextCourse = new Course(parsedCourse);
                 database.Add(nextCourse);
             }
         }
