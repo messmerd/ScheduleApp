51888cc2ee808aed660a44e1e6e08861e21ccdb1
GCC\FEHRTJ1 <fehrtj1@gcc.edu>
2018-03-19 20:10:18 -0400

    adjustments

--- ScheduleApp/ScheduleApp/Form1.cs
+++ ScheduleApp/ScheduleApp/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace ScheduleApp
 {
-    public partial class Form1 : Form
+    public partial class masterContainer : Form
     {
-        public Form1()
+        public masterContainer()
         {
             InitializeComponent();
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void advSearchBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
