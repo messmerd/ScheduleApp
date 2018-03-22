6cc77b6a0348aa628a34df79d50ba493cd7b8d73
Michael Bender <bendermc1@gcc.edu>
2018-03-22 19:28:56 -0400

    Continued work on parsing the imported files(almost done)

--- ScheduleApp/ScheduleApp/Course.cs
+++ ScheduleApp/ScheduleApp/Course.cs
@@ -10,10 +10,10 @@ namespace CourseClass
     public struct Course
     {
         public Build building;
-        public int room;
+        public string room;
 
         public string courseDept;
-        public int courseNum;
+        public string courseNum;
         public string courseSect;
         public string courseCode;// = courseDept + courseNum.toString();
 
@@ -23,10 +23,12 @@ namespace CourseClass
         public int enrollment;
         public int capacity;
 
-        public List<Tuple<double, double>> time;
+        List<bool> day;
+        public Tuple<double, double> time;
 
         public Tuple<string, string> professor;
 
+        int credits;
 
         private int courseID;
 
@@ -36,19 +38,21 @@ namespace CourseClass
 
             this.professor = Tuple.Create<string, string>(null, null);
 
-            this.time = new List<Tuple<double, double>> {
-                Tuple.Create<double, double>(0, 0),
-                Tuple.Create<double, double>(0, 0),
-                Tuple.Create<double, double>(0, 0),
-                Tuple.Create<double, double>(0, 0),
-                Tuple.Create<double, double>(0, 0)
+            this.time = Tuple.Create<double, double>(0, 0);
+            this.day = new List<bool>
+            {
+                false,
+                false,
+                false,
+                false,
+                false
             };
 
             this.building = Build.NONE;
-            this.room = 0;
+            this.room = "0";
 
             courseDept = "";
-            courseNum = 0;
+            courseNum = "0";
             courseSect = "A";
             courseCode = courseDept + " " + courseNum + " " + courseSect;
 
@@ -57,37 +61,70 @@ namespace CourseClass
 
             enrollment = 0;
             capacity = 0;
+
+            credits = 0;
         }
 
         public Course(List<string> parsedCourse) // Constructor
         {
             this.courseID = 0;
             
-            this.professor = Tuple.Create<string, string>(null, null);
+            this.professor = Tuple.Create<string, string>(parsedCourse[11], parsedCourse[12]);
 
-            this.time = new List<Tuple<double, double>> {
-                Tuple.Create<double, double>(0, 0),
-                Tuple.Create<double, double>(0, 0),
-                Tuple.Create<double, double>(0, 0),
-                Tuple.Create<double, double>(0, 0),
-                Tuple.Create<double, double>(0, 0)
+            //parsedCourse[3], parsedCourse[4]
+            //s.Split(':')[3]
+            int j, t1, t2;
+            string s1, s2;
+
+            s1 = parsedCourse[3].Split(':')[0];
+            s2 = parsedCourse[3].Split(':')[1];
+            if (Int32.TryParse(s1, out j))
+                t1 = j;
+            else
+                t1 = 0;
+
+
+            if (Int32.TryParse(parsedCourse[4], out j))
+                t2 = j;
+            else
+                t2 = 0;
+            this.time = Tuple.Create<double, double>(t1, t2);
+            this.day = new List<bool>
+            {
+                parsedCourse[5].Contains("M"),
+                parsedCourse[5].Contains("T"),
+                parsedCourse[5].Contains("W"),
+                parsedCourse[5].Contains("R"),
+                parsedCourse[5].Contains("F")
             };
 
             Build result;//BAO, HAL, HH, OFFCP, PFAC, PLC, RH, RO, STEM, TBD, NONE
             if (Enum.TryParse(parsedCourse[5], out result)) { building = result; }//fix building loc
             else { building = Build.NONE; }
-            this.room = 0;
+            this.room = parsedCourse[7];
 
-            courseDept = "";
-            courseNum = 0;
-            courseSect = "A";
+            // parsedCourse[0]
+            courseDept = System.Text.RegularExpressions.Regex.Split(parsedCourse[0], @"\s+")[0];
+            courseNum = System.Text.RegularExpressions.Regex.Split(parsedCourse[0], @"\s+")[1];
+            courseSect = System.Text.RegularExpressions.Regex.Split(parsedCourse[0], @"\s+")[2];
             courseCode = courseDept + " " + courseNum + " " + courseSect;
 
-            shortName = "";
-            longName = "";
+            shortName = parsedCourse[2];
+            longName = parsedCourse[3];
 
-            enrollment = 0;
-            capacity = 0;
+            if (Int32.TryParse(parsedCourse[8], out j))
+                enrollment = j;
+            else
+                enrollment = 0;
+            if (Int32.TryParse(parsedCourse[9], out j))
+                capacity = j;
+            else
+                capacity = 0;
+
+            if (Int32.TryParse(parsedCourse[10], out j))
+                credits = j;
+            else
+                credits = 0;
         }
 
         public int getCourseID()  // Note: courseID is NOT the same as courseCode 
