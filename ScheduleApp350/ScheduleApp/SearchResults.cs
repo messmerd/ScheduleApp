87dbaf110ee63a388b206523cb1f0cc5e0d52501
messmerd <messmerd97@gmail.com>
2018-04-24 19:56:29 -0400

    Improved the course sorting, created alphabetize function so that if you want to sort by credits for example, it will sort by credits but all courses with the smae number of credits will be sorted alphabetically.

--- ScheduleApp350/ScheduleApp/SearchResults.cs
+++ ScheduleApp350/ScheduleApp/SearchResults.cs
@@ -58,8 +58,11 @@ namespace SearchResultsClass
                 case SORTTYPE.COURSECODE:  // Course code
                     courses = courses.OrderBy(x => x.getCourseCode()).ToList();
                     break;
+                case SORTTYPE.DAY:
+                    courses = courses.OrderBy(x => Int32.MaxValue - boolArrayToInt(x.getDay().ToArray())).ToList();
+                    break; 
                 case SORTTYPE.RELEVANCY:
-                    sortByRelevance();
+                    courses.Sort((c1, c2) => relevance[c2.getCourseID()].CompareTo(relevance[c1.getCourseID()])); // Sort by relevancy
                     break;
 
                 default:
@@ -67,8 +70,9 @@ namespace SearchResultsClass
             }
 
             if (!ascending)
-                courses.Reverse(); 
-            
+                courses.Reverse();
+
+            alphabetize(sortBy);
         }
 
         private void sortByRelevance()
@@ -103,6 +107,73 @@ namespace SearchResultsClass
 
                 }
             }
+        }
+
+        private Int32 boolArrayToInt(bool[] input)
+        {
+            System.Collections.BitArray arr = new System.Collections.BitArray(input);
+            byte[] data = new byte[4];
+            arr.CopyTo(data, 0);
+            return BitConverter.ToInt32(data, 0);
+        }
+
+        private double getSortableValue(SORTTYPE type, int index)
+        {
+            switch (type)
+            {
+                case SORTTYPE.CREDITS:
+                    return (double)courses[index].getCredits();
+                case SORTTYPE.DAY:
+                    return (double)(Int32.MaxValue - boolArrayToInt(courses[index].getDay().ToArray()));
+                case SORTTYPE.PROF:
+                    return (double)CourseInfoClass.CourseInfo.Create().prof_database.FindIndex(x => x.Equals(courses[index].getProf()));
+                case SORTTYPE.STARTTIME:
+                    return courses[index].getTime().Item1;
+                case SORTTYPE.ENDTIME:
+                    return courses[index].getTime().Item2;
+                case SORTTYPE.RELEVANCY:
+                    return (double)relevance[courses[index].getCourseID()]; 
+
+                default:
+                    return -1.0;
+            }
+
+        }
+
+        private void alphabetize(SORTTYPE groupBy)
+        {
+            // This part orders the results (for each different relevancy value) by department code and course number: 
+            if (courses.Count > 1) // Only needs to sort if there is more than one result 
+            {
+                int start = 0;
+                int currentRelevance = relevance[courses[0].getCourseID()];
+
+                double currentValue = getSortableValue(groupBy, 0);
+
+                List<Course> sortedSubset = new List<Course>();
+
+                for (int i = 0; i < courses.Count; i++)
+                {
+                    if (i == courses.Count - 1 || getSortableValue(groupBy, i + 1) != currentValue)
+                    {
+                        sortedSubset = courses.GetRange(start, i - start + 1).OrderBy(a => a.getCourseID()).ToList();  // Sort subset by department/course number
+
+                        for (int k = start; k < i + 1; k++) // Copies the sorted subset back into the main list 
+                        {
+                            courses[k] = sortedSubset[k - start];
+                        }
+
+                        if (i != courses.Count - 1)  // If it isn't the last element 
+                        {
+                            currentValue = getSortableValue(groupBy, i + 1);
+                            start = i + 1;
+                        }
+                    }
+
+                }
+            }
+
+
         }
 
     }
