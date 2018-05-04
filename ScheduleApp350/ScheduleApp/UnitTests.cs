08e81aea6658f0f4ec9f7c0cd556f5881b6b6a7f
GCC\OBERLANDERAR1 <oberlanderar1>
2018-05-04 14:26:46 -0400

    Test 1 beginning work.

--- ScheduleApp350/ScheduleApp/UnitTests.cs
+++ ScheduleApp350/ScheduleApp/UnitTests.cs
@@ -9,8 +9,11 @@ namespace ScheduleApp
     public class UnitTests
     {
         //all tests are based on requirement 7 (professor searching) of the sprint 1 testing plan, tests b-d
-        public bool test1() //searching for a specific teacher
+        public bool test1() //searching for a specific teacher; courseID's are 184, 186, 187
         {
+            Search search = Search.Create("course_database.txt");
+            CourseInfo DB = CourseInfo.Create();
+            search.searchForQuery(null);
             /*
 
             if(results == )
@@ -25,7 +28,7 @@ namespace ScheduleApp
             return true;
         }
 
-        public bool test2() //search databse with nothing entered (all courses in database will be returned)
+        public bool test2() //search databse with nothing entered (all courses in database will be returned); course ID's are 0 to 760
         {
             /*
             if(results == )
@@ -40,7 +43,7 @@ namespace ScheduleApp
             return true;
         }
 
-        public bool test3() //search for professor while something is written in regular search box
+        public bool test3() //search for professor while Software is written in regular search box; courseID's are 186, 187
         {
             /*
             if(results == )
